803cbb7b080918c0aea503798cd83b9c07fd0188
Nik <nikitama1hr@gmail.com>
2023-06-02 00:24:02 +0300

    draft add studentCount option filters

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # T-MyRoom
 
-## How improve 
+## How improve
+
 * install eslint / prettier / husky / editorconfig
-* write docs 
-* create scheme  
+* write docs
+* create scheme
 * add some bugs :D
+* I want to create one req to DB, because we used flexible settings. But I understood, you'll want to see more req. DB 
+* Break the methods into separate ones and iterate over the structure 
--- src/controllers/lessonController.js
+++ src/controllers/lessonController.js
@@ -15,6 +15,101 @@ const getLesson = async (req, res) => {
     console.log('REQ QUERY', date, status, teacherIds, studentsCount, page, lessonPerPage)
 
 
+    // @data req.query only date && status && teacherIds && studentsCount
+    if (date !== undefined &&
+        status !== undefined &&
+        teacherIds !== undefined &&
+        studentsCount !== undefined &&
+        page === undefined &&
+        lessonPerPage === undefined) {
+
+        console.log('\x1b[34m', '@data req.query only date && status && teacherIds && studentsCount' + '\x1b[0m')
+
+        console.log(date.split(',').length === 2 && studentsCount.split(',').length === 2)
+        if (date.split(',').length === 2 && studentsCount.split(',').length === 2) {
+            const teachersData = await db.query(
+                '  SELECT *\n' +
+                '  FROM lesson_teachers\n' +
+                '  INNER JOIN lessons ON lesson_teachers.lesson_id = lessons.id\n' +
+                '  INNER JOIN teachers ON lesson_teachers.teacher_id = teachers.id\n' +
+                '  WHERE lessons.date BETWEEN $1 AND $2 AND status = $3 AND teacher_id = $4'
+                ,
+                [date.split(',')[0], date.split(',')[1], status, teacherIds]
+            )
+            const studentsData = await db.query(
+                'SELECT *\n' +
+                'FROM (\n' +
+                '  SELECT *\n' +
+                '  FROM (\n' +
+                '    SELECT lesson_id, date, COUNT(*) AS row_count\n' +
+                '    FROM (\n' +
+                '      SELECT lesson_id, student_id, visit, date, status, name\n' +
+                '      FROM lesson_students\n' +
+                '      INNER JOIN lessons ON lesson_students.lesson_id = lessons.id\n' +
+                '      INNER JOIN students ON lesson_students.student_id = students.id\n' +
+                '      WHERE lessons.date BETWEEN $1 AND $2 AND status = $3\n' +
+                '    ) subquery\n' +
+                '    GROUP BY lesson_id, date\n' +
+                '  ) row_count_subquery\n' +
+                '  JOIN lesson_students ON row_count_subquery.lesson_id = lesson_students.lesson_id\n' +
+                '  JOIN students  on lesson_students.student_id = students.id\n' +
+                '  WHERE row_count BETWEEN $4 AND $5\n' +
+                ') select_all_students;\n'
+                ,
+                [date.split(',')[0], date.split(',')[1], status, studentsCount.split(',')[0], studentsCount.split(',')[1]]
+            )
+
+            const readyData = configObject(teachersData.rows, studentsData.rows)
+            console.log(studentsData.rows)
+        }
+
+
+        if (date.split(',').length === 1 && studentsCount.split(',').length === 1) {
+            const teachersData = await db.query(
+                '  SELECT lesson_id, teacher_id,  date, title, status, name\n' +
+                '  FROM lesson_teachers\n' +
+                '  INNER JOIN lessons ON lesson_teachers.lesson_id = lessons.id\n' +
+                '  INNER JOIN teachers ON lesson_teachers.teacher_id = teachers.id\n' +
+                '  WHERE lessons.date = $1 AND status=$2 AND teacher_id = $3'
+                ,
+                [date.split(',')[0], status, teacherIds]
+            )
+
+            const studentsData = await db.query(
+                'SELECT *\n' +
+                'FROM (\n' +
+                '  SELECT *\n' +
+                '  FROM (\n' +
+                '    SELECT lesson_id, date, COUNT(*) AS row_count\n' +
+                '    FROM (\n' +
+                '      SELECT lesson_id, student_id, visit, date, status, name\n' +
+                '      FROM lesson_students\n' +
+                '      INNER JOIN lessons ON lesson_students.lesson_id = lessons.id\n' +
+                '      INNER JOIN students ON lesson_students.student_id = students.id\n' +
+                '      WHERE lessons.date = $1 AND status = $2' +
+                '    ) subquery\n' +
+                '    GROUP BY lesson_id, date\n' +
+                '  ) row_count_subquery\n' +
+                '  JOIN lesson_students ON row_count_subquery.lesson_id = lesson_students.lesson_id\n' +
+                '  JOIN students  on lesson_students.student_id = students.id\n' +
+                '  WHERE row_count = $3' +
+                ') select_all_students;\n'
+                ,
+                [date.split(',')[0], status, studentsCount]
+            )
+
+            // TODO: Rewrite on sql (EXPAND SQL QUERY)
+            console.log(studentsData.rows)
+
+
+            const readyData = configObject(teachersData.rows, studentsData.rows)
+            console.log('============================')
+            return res.send(readyData)
+        }
+
+    }
+
+
     // @data req.query only date && status && teacherIds
     if (date !== undefined &&
         status !== undefined &&
@@ -23,33 +118,63 @@ const getLesson = async (req, res) => {
         page === undefined &&
         lessonPerPage === undefined) {
 
+        console.log('\x1b[34m', '@data req.query only date && status && teacherIds' + '\x1b[0m')
+
 
         if (date.split(',').length === 2) {
-
             const teachersData = await db.query(
                 '  SELECT *\n' +
                 '  FROM lesson_teachers\n' +
                 '  INNER JOIN lessons ON lesson_teachers.lesson_id = lessons.id\n' +
                 '  INNER JOIN teachers ON lesson_teachers.teacher_id = teachers.id\n' +
-                '  WHERE lesson_teachers.lesson_id = $1',
-                [teacherIds]
+                '  WHERE lessons.date BETWEEN $1 AND $2 AND status = $3 AND teacher_id = $4'
+                ,
+                [date.split(',')[0], date.split(',')[1], status, teacherIds]
             )
 
+            const studentsData = await db.query(
+                '\n' +
+                '  SELECT *\n' +
+                '  FROM lesson_students\n' +
+                '  INNER JOIN lessons ON lesson_students.lesson_id = lessons.id\n' +
+                '  INNER JOIN students ON lesson_students.lesson_id = students.id\n' +
+                '  WHERE lessons.date BETWEEN $1 AND $2 AND status = $3'
+                ,
+                [date.split(',')[0], date.split(',')[1], status]
+            )
+
+            const readyData = configObject(teachersData.rows, studentsData.rows)
+            console.log(readyData)
         }
+
 
         if (date.split(',').length === 1) {
-
             const teachersData = await db.query(
-                '  SELECT *\n' +
+                '  SELECT lesson_id, teacher_id,  date, title, status, name\n' +
                 '  FROM lesson_teachers\n' +
                 '  INNER JOIN lessons ON lesson_teachers.lesson_id = lessons.id\n' +
                 '  INNER JOIN teachers ON lesson_teachers.teacher_id = teachers.id\n' +
-                '  WHERE lesson_teachers.teacher_id = $1',
-                [teacherIds]
+                '  WHERE lessons.date = $1 AND status=$2 AND teacher_id = $3'
+                ,
+                [date.split(',')[0], status, teacherIds]
             )
-            console.log(teachersData.rows)
 
+            const studentsData = await db.query(
+                '\n' +
+                '  SELECT  lesson_id, student_id, visit, date, status, name\n' +
+                '  FROM lesson_students\n' +
+                '  INNER JOIN lessons ON lesson_students.lesson_id = lessons.id\n' +
+                '  INNER JOIN students ON lesson_students.student_id = students.id\n' +
+                '  WHERE lessons.date = $1 AND status=$2'
+                ,
+                [date.split(',')[0], status]
+            )
+            console.log(teachersData.rows, studentsData.rows)
+            const readyData = configObject(teachersData.rows, studentsData.rows)
+            console.log('============================')
+            return res.send(readyData)
         }
+
     }
 
 
@@ -61,23 +186,63 @@ const getLesson = async (req, res) => {
         page === undefined &&
         lessonPerPage === undefined) {
 
+        console.log('\x1b[34m', '@data req.query only date && status' + '\x1b[0m')
+
 
         if (date.split(',').length === 2) {
-            const dat = await db.query(
-                'SELECT * FROM lessons WHERE date BETWEEN $1 AND $2 AND status=$3',
+            const teachersData = await db.query(
+                '  SELECT *\n' +
+                '  FROM lesson_teachers\n' +
+                '  INNER JOIN lessons ON lesson_teachers.lesson_id = lessons.id\n' +
+                '  INNER JOIN teachers ON lesson_teachers.teacher_id = teachers.id\n' +
+                '  WHERE lessons.date BETWEEN $1 AND $2 AND status = $3'
+                ,
                 [date.split(',')[0], date.split(',')[1], status]
             )
 
+            const studentsData = await db.query(
+                '\n' +
+                '  SELECT *\n' +
+                '  FROM lesson_students\n' +
+                '  INNER JOIN lessons ON lesson_students.lesson_id = lessons.id\n' +
+                '  INNER JOIN students ON lesson_students.lesson_id = students.id\n' +
+                '  WHERE lessons.date BETWEEN $1 AND $2 AND status = $3'
+                ,
+                [date.split(',')[0], date.split(',')[1], status]
+            )
+
+            const readyData = configObject(teachersData.rows, studentsData.rows)
+            console.log(readyData)
         }
+
 
         if (date.split(',').length === 1) {
-            const dat = await db.query(
-                'SELECT * FROM lessons WHERE date = $1 AND status=$2',
+            const teachersData = await db.query(
+                '  SELECT lesson_id, teacher_id,  date, title, status, name\n' +
+                '  FROM lesson_teachers\n' +
+                '  INNER JOIN lessons ON lesson_teachers.lesson_id = lessons.id\n' +
+                '  INNER JOIN teachers ON lesson_teachers.teacher_id = teachers.id\n' +
+                '  WHERE lessons.date = $1 AND status=$2'
+                ,
                 [date.split(',')[0], status]
             )
-            console.log(dat.rows)
 
+            const studentsData = await db.query(
+                '\n' +
+                '  SELECT  lesson_id, student_id, visit, date, status, name\n' +
+                '  FROM lesson_students\n' +
+                '  INNER JOIN lessons ON lesson_students.lesson_id = lessons.id\n' +
+                '  INNER JOIN students ON lesson_students.student_id = students.id\n' +
+                '  WHERE lessons.date = $1 AND status=$2'
+                ,
+                [date.split(',')[0], status]
+            )
+
+            const readyData = configObject(teachersData.rows, studentsData.rows)
+            console.log('============================')
+            return res.send(readyData)
         }
+
     }
 
     // @data req.query only date
@@ -87,7 +252,7 @@ const getLesson = async (req, res) => {
         studentsCount === undefined &&
         page === undefined &&
         lessonPerPage === undefined) {
-        console.log('@data req.query only date')
+        console.log('\x1b[34m', '@data req.query only date' + '\x1b[0m')
 
 
         if (date.split(',').length === 2) {
@@ -118,7 +283,7 @@ const getLesson = async (req, res) => {
 
 
         if (date.split(',').length === 1) {
-            const teachersData  = await db.query(
+            const teachersData = await db.query(
                 '  SELECT lesson_id, teacher_id,  date, title, status, name\n' +
                 '  FROM lesson_teachers\n' +
                 '  INNER JOIN lessons ON lesson_teachers.lesson_id = lessons.id\n' +
